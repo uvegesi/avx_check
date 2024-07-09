6ed1786a845c695b46fc6737f6fbab8cb4614d23
Istvan Uveges <uveges.istvan@gmail.com>
2024-07-09 13:26:02 +0200

    Changed urls list

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
 urls = ['https://avxforum.hu/forum/viewforum.php?f=64',
-            'https://avxforum.hu/forum/viewforum.php?f=64&sid=70d662223359609be8192b6e76c25195&start=25',
-            'https://avxforum.hu/forum/viewforum.php?f=64&&start=50'
-            ]
+        'https://avxforum.hu/forum/viewforum.php?f=64&sid=70d662223359609be8192b6e76c25195&start=25',
+        'https://avxforum.hu/forum/viewforum.php?f=64&&start=50'
+        ]
 
 email_user = os.environ["EMAIL_FROM"]
 email_password = os.environ["SOME_SECRET"]

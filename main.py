0429557fffff383f4396753a51ae6bcc6254ddf3
Istvan Uveges <uveges.istvan@gmail.com>
2024-07-20 19:10:01 +0200

    Updated code to use SSL

--- main.py
+++ main.py
@@ -36,16 +36,13 @@ def send_email(subject, body):
     msg.attach(MIMEText(body, 'plain'))
 
     try:
-        print("heeeeeeeeeeeeeeeeeeeee")
-        server = smtplib.SMTP('smtp.mail.yahoo.com', 587)  # Replace with your SMTP server
-        print('do we get here?')
-        server.starttls()
-        server.login(config.email_user, config.email_password)
-        print('user: ', config.email_user, 'Passw: ', config.email_password)
-        print('user: ', os.environ['EMAIL_USER'], 'Passw: ', os.environ['EMAIL_PASSWORD'])
+        print("Starting SSL connection to SMTP server...")
+        server = smtplib.SMTP_SSL('smtp.mail.yahoo.com', 465)  # Connect using SSL
+        server.login(os.environ['EMAIL_USER'], os.environ['EMAIL_PASSWORD'])
+        print('Logged in as:', os.environ['EMAIL_USER'])
         text = msg.as_string()
-        print('message:', text)
-        server.sendmail(config.email_user, config.email_to, text)
+        print('Message content:', text)
+        server.sendmail(os.environ['EMAIL_USER'], os.environ['EMAIL_TO'], text)
         server.quit()
         print('Email sent successfully')
     except Exception as e:

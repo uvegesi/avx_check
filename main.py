27509a95f2cde128556d3a35fb081cfb2f6f19a0
Istvan Uveges <uveges.istvan@gmail.com>
2024-07-20 19:20:33 +0200

    And more debug..

--- main.py
+++ main.py
@@ -35,20 +35,7 @@ def send_email(subject, body):
     msg['Subject'] = subject
     msg.attach(MIMEText(body, 'plain'))
 
-    try:
-        print("heeeeeeeeeeeeeeeeeeeee")
-        server = smtplib.SMTP('smtp.mail.yahoo.com', 587)  # Replace with your SMTP server
-        print('do we get here?')
-        server.starttls()
-        server.login(config.email_user, config.email_password)
-        print('user: ', config.email_user, 'Passw: ', config.email_password)
-        print('user: ', os.environ['EMAIL_USER'], 'Passw: ', os.environ['EMAIL_PASSWORD'])
-        text = msg.as_string()
-        print('message:', text)
-        server.sendmail(config.email_user, config.email_to, text)
-        server.quit()
-        print('Email sent successfully')
-        
+    try:     
         print("Starting SSL connection to SMTP server...")
         server = smtplib.SMTP_SSL('smtp.mail.yahoo.com', 465)  # Connect using SSL
         server.login(config.email_user, config.email_password)
